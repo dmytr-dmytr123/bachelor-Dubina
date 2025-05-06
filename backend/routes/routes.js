3c90714a438a20b994aa8b47d155d617b4bdb8e8
dmytrodubina <dmytrodubina@outlook.com>
2025-05-06 03:05:02 +0300

    routes

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,7 +1,5 @@
-const router = require("express").Router();
+const express = require('express');
+const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Api is up and running");
-});
 
 module.exports = router;
